c69ab0f2aa4036b77e6fa7b4824c1109921e653c
Marcus Rainbow <40654783+MarcusRainbow@users.noreply.github.com>
2019-12-04 19:34:24 +0000

    Support all expressions
    Added support for all expression nodes, with a few caveats:
    
    * only works for numeric types
    * no support for *in* or *is* keywords
    
    Seems to get the precedence right, though I've not hammered it hard,

---
+++ add_mult.py
@@ -0,0 +1,32 @@
+def add_mult(a: int, b: int, c: int) -> int:
+    return a + b * c
+
+def sub_div(a: int, b: int, c: int) -> int:
+    return a - b / c
+
+def floor_div(a: int, b: int, c: int) -> int:
+    return a // (b % c)
+
+def bitwise(a: int, b: int, c: int) -> int:
+    return (a << b) & (b >> c) ^ (a | b)
+
+def powers(a: int, b: int, c: int) -> int:
+    return a * (a + b) ** ((b + c) ** (c + a))
+
+def unaries(a: int, b: int, c: int) -> int:
+    return (not (~a + +b + -c)) + ~~a
+
+def boolean(a: bool, b: bool, c: bool) -> bool:
+    return (a and b and c) or not (a or b or c)
+
+def compare(a: int, b: int, c: int) -> bool:
+    return (a == b) or (b != c) or (a > b) or (c < a) or (a <= b) or (c >= b)
+
+def precedence(a: int, b: int, c: int) -> bool:
+    return a + (b - c) + (b + c) * a
+
+def multi_compare(a: int, b: int, c: int) -> bool:
+    return a < b < c or a >= b > c or a != b == c
+
+def boolean_precedence(a: bool, b: bool, c: bool) -> bool:
+    return a and b or c and b or a and c
--- compiler.py
+++ compiler.py
@@ -9,6 +9,28 @@ from enum import Enum
 
 OPEN_BRACE = '{'
 CLOSE_BRACE = '}'
+ALLOWED_BINARY_OPERATORS = { "Add", "Mult", "Sub", "Div", "FloorDiv",
+    "Mod", "LShift", "RShift", "BitOr", "BitXor", "BitAnd" }
+ALLOWED_COMPARISON_OPERATORS = { "Eq", "NotEq", "Lt", "LtE", "Gt", "GtE" }
+
+# Fortunately, the precedence of Python operators is the same as Rust,
+# except for ** (doesn't exist in Rust), is/in (don't exist in Rust)
+# "not", which is very highest precedence in Rust, but just above the
+# other boolean operators in Rust.
+OPERATOR_PRECEDENCE = {
+    "Pow": 12,
+    "UAdd": 11, "USub": 11, "Invert": 11, "Not": 11,
+    "Mult": 10, "Div": 10, "FloorDiv": 10, "Mod": 10,
+    "Add": 9, "Sub": 9,
+    "LShift": 8, "RShift": 8,
+    "BitAnd": 7,
+    "BitXor": 6,
+    "BitOr": 5,
+    "Eq": 4, "NotEq": 4, "Gt": 4, "GtE": 4, "Lt": 4, "LtE": 4,
+    # "Not": 3, (this would be right for Python, but not for Rust)
+    "And": 2,
+    "Or": 1,
+}
 
 def type_from_annotation(annotation: str, arg: str) -> str:
     if annotation is None:
@@ -17,6 +39,8 @@ def type_from_annotation(annotation: str, arg: str) -> str:
     id = annotation.id
     if id == 'int':
         return 'u32'
+    elif id == 'bool':
+        return 'bool'
     elif id == 'str':
         return 'str'
     elif id == 'num':
@@ -30,12 +54,27 @@ class TreeWalker(ast.NodeVisitor):
     def __init__(self):
         self.indent = 0
         self.next_separator = ""
+        self.precedence = 0
 
     def pretty(self):
         return '    ' * self.indent
     
     def add_pretty(self, increment: int):
         self.indent += increment
+
+    def parens_if_needed(self, op: str, visit):
+        # use precedence * 2 so we can add one to control less than or equal
+        prec = OPERATOR_PRECEDENCE[op] * 2
+        if prec < self.precedence:
+            print("(", end='')
+
+        old_prec = self.precedence
+        self.precedence = prec
+        visit()
+        self.precedence = old_prec
+
+        if prec < self.precedence:
+            print(")", end='')
 
     def visit_FunctionDef(self, node):
         # function name
@@ -94,13 +133,152 @@ class TreeWalker(ast.NodeVisitor):
         print(f'"{node.s}"', end='')
 
     def visit_BinOp(self, node):
-        self.generic_visit(node)
+        # some binary operators such as '+' translate
+        # into binary operators in Rust
+        op = node.op.__class__.__name__
+        if op in ALLOWED_BINARY_OPERATORS:
+            self.parens_if_needed(op, lambda: self.do_visit_BinOp(node))
+        else:
+            old_prec = self.precedence
+            self.precedence = 0     # these parentheses reset everything
+
+            self.visit(node.op)
+            print("(", end='')
+            self.visit(node.left)
+            print(", ", end='')
+            self.visit(node.right)
+            print(")", end='')
+
+            self.precedence = old_prec
     
+    def do_visit_BinOp(self, node):
+        self.visit(node.left)
+        self.visit(node.op)
+        self.precedence += 1    # left to right associative
+        self.visit(node.right)
+        self.precedence -= 1
+
     def visit_Add(self, node):
         print(" + ", end='')
     
     def visit_Mult(self, node):
         print(" * ", end='')
+
+    def visit_Sub(self, node):
+        print(" - ", end='')
+    
+    def visit_Div(self, node):
+        # print("warning: floating point division", file=sys.stderr)
+        print(" / ", end='')
+
+    def visit_FloorDiv(self, node):
+        # print("warning: integer division", file=sys.stderr)
+        print(" / ", end='')
+
+    def visit_Mod(self, node):
+        # print("warning: Python mod operator is different from Rust")
+        print(" % ", end='')
+
+    def visit_Pow(self, node):
+        print("pow", end='')
+
+    def visit_LShift(self, node):
+        print(" << ", end='')
+
+    def visit_RShift(self, node):
+        print(" >> ", end='')    
+
+    def visit_BitOr(self, node):
+        print(" | ", end='')
+
+    def visit_BitXor(self, node):
+        print(" ^ ", end='')
+
+    def visit_BitAnd(self, node):
+        print(" & ", end='')
+    
+    def visit_UnaryOp(self, node):
+        op = node.op.__class__.__name__
+        self.parens_if_needed(op, lambda: self.generic_visit(node))
+
+    def visit_UAdd(self, node):
+        print("+", end='')
+
+    def visit_USub(self, node):
+        print("-", end='')
+
+    def visit_Not(self, node):
+        print("!", end='')
+
+    def visit_Invert(self, node):
+        print("~", end='')
+
+    def visit_BoolOp(self, node):
+        op = node.op.__class__.__name__
+        self.parens_if_needed(op, lambda: self.do_visit_BoolOp(node))
+
+    def do_visit_BoolOp(self, node):
+        """
+        Invoked by visit_BoolOp to do the work apart from the parens
+        """
+        first = True
+        for v in node.values:
+            if not first:
+                self.visit(node.op)
+            self.visit(v)
+            first = False
+
+    def visit_And(self, node):
+        print(" && ", end='')
+
+    def visit_Or(self, node):
+        print(" || ", end='')
+
+    def visit_Compare(self, node):
+        """
+        Invoked for any comparison operator such as <, >, ==.
+
+        Note that multiple comparisons in Rust are very different from
+        Python. In Rust, it is not permissible to write "a X b Y c"
+        where X and Y are comparison operators (possibly the same one).
+        In Python, this is shorthand for "(a X b) and (b Y c). We
+        therefore expand it like this in the Rust.
+        """
+        op_len = len(node.ops)
+        assert(op_len == len(node.comparators))
+        if op_len > 1:
+            print("(", end='')
+
+        self.visit(node.left)
+        for op, c, i in zip(node.ops, node.comparators, range(op_len)):
+            # we do not yet handle is or in
+            assert(op.__class__.__name__ in ALLOWED_COMPARISON_OPERATORS)
+            self.visit(op)
+            self.visit(c)
+            if op_len > 1:
+                if i != op_len - 1:
+                    print(") && (", end='')
+                    self.visit(c)
+                else:
+                    print(")", end='')
+
+    def visit_Eq(self, node):
+        print(" == ", end='')
+    
+    def visit_NotEq(self, node):
+        print(" != ", end='')
+
+    def visit_Lt(self, node):
+        print(" < ", end='')
+    
+    def visit_LtE(self, node):
+        print(" <= ", end='')
+
+    def visit_Gt(self, node):
+        print(" > ", end='')
+    
+    def visit_GtE(self, node):
+        print(" >= ", end='')
 
 def compile_to_rust(source, filename: str) -> bool:
     """
